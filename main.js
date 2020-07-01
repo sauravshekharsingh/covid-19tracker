88ae74247cc3d08876fd4a5cf39fc25881570c41
Saurav Shekhar Singh <62594900+sauravshekharsingh@users.noreply.github.com>
2020-07-01 23:54:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-$.getJSON("https://api.covid19api.com/summary",function(data){
-	var date = data.Date;
+$.getJSON("https://api.covid19api.com/summary",function(data1){
+	var date = data1.Date;
 	$(".updated-date").append(" " + date.slice(0,10));
 	$(".updated-time").append(" " + date.slice(11,19));
-	$(".confirmed-no").append(numberWithCommas(data.Global.TotalConfirmed));
-	$(".death-no").append(numberWithCommas(data.Global.TotalDeaths));
-	$(".recovered-no").append(numberWithCommas(data.Global.TotalRecovered));
-	$(".confirmed-no-new").append(numberWithCommas(data.Global.NewConfirmed));
-	$(".death-no-new").append(numberWithCommas(data.Global.NewDeaths));
-	$(".recovered-no-new").append(numberWithCommas(data.Global.NewRecovered));
-	var totalCountries = data.Countries.length;
+	$(".confirmed-no").append(numberWithCommas(data1.Global.TotalConfirmed));
+	$(".death-no").append(numberWithCommas(data1.Global.TotalDeaths));
+	$(".recovered-no").append(numberWithCommas(data1.Global.TotalRecovered));
+	$(".confirmed-no-new").append(numberWithCommas(data1.Global.NewConfirmed));
+	$(".death-no-new").append(numberWithCommas(data1.Global.NewDeaths));
+	$(".recovered-no-new").append(numberWithCommas(data1.Global.NewRecovered));
+	var totalCountries = data1.Countries.length;
 	for(i=0 ; i<totalCountries ; i++){
-		var country = data.Countries[i].Country;
+		var country = data1.Countries[i].Country;
 		if(country=="India"){
 			break;
 		}
 	}
-	$(".confirmed-no-india").append(numberWithCommas(data.Countries[i].TotalConfirmed));
-	$(".death-no-india").append(numberWithCommas(data.Countries[i].TotalDeaths));
-	$(".recovered-no-india").append(numberWithCommas(data.Countries[i].TotalRecovered));
-	$(".confirmed-no-india-new").append(numberWithCommas(data.Countries[i].NewConfirmed));
-	$(".death-no-india-new").append(numberWithCommas(data.Countries[i].NewDeaths));
-	$(".recovered-no-india-new").append(numberWithCommas(data.Countries[i].NewRecovered));
+	$(".confirmed-no-india").append(numberWithCommas(data1.Countries[i].TotalConfirmed));
+	$(".death-no-india").append(numberWithCommas(data1.Countries[i].TotalDeaths));
+	$(".recovered-no-india").append(numberWithCommas(data1.Countries[i].TotalRecovered));
+	$(".confirmed-no-india-new").append(numberWithCommas(data1.Countries[i].NewConfirmed));
+	$(".death-no-india-new").append(numberWithCommas(data1.Countries[i].NewDeaths));
+	$(".recovered-no-india-new").append(numberWithCommas(data1.Countries[i].NewRecovered));
 });
 
 $.getJSON("https://api.covid19india.org/data.json",function(data2){
